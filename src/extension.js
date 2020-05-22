6f31d5120060584b86d77cff95b4d59ddcf2ad5d
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2020-05-23 01:25:33 +0530

    Initial commit from "GmailJS Node Boilerplate" clone

---
+++ src/extension.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// loader-code: wait until gmailjs has finished loading, before triggering actual extensiode-code.
+const loaderId = setInterval(() => {
+    if (!window._gmailjs) {
+        return;
+    }
+
+    clearInterval(loaderId);
+    startExtension(window._gmailjs);
+}, 100);
+
+// actual extension-code
+function startExtension(gmail) {
+    console.log("Extension loading...");
+    window.gmail = gmail;
+
+    gmail.observe.on("load", () => {
+        const userEmail = gmail.get.user_email();
+        console.log("Hello, " + userEmail + ". This is your extension talking!");
+
+        gmail.observe.on("view_email", (domEmail) => {
+            console.log("Looking at email:", domEmail);
+            const emailData = gmail.new.get.email_data(domEmail);
+            console.log("Email data:", emailData);
+        });
+    });
+}
